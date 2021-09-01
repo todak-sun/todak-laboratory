dd370927829d9eeb42c237f41006dcdaa68b1073
todak-sun <tjsdydwn@gmail.com>
2021-09-01 23:09:40 +0900

    jts test with postgis start

--- jts-tutorial/src/main/java/PointTest.java
+++ jts-tutorial/src/main/java/PointTest.java
@@ -1,8 +1,7 @@
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.PrecisionModel;
+import org.locationtech.jts.geom.*;
 import org.locationtech.jts.geom.impl.CoordinateArraySequence;
+import org.locationtech.jts.geom.impl.CoordinateArraySequenceFactory;
+import org.locationtech.jts.geom.util.GeometryEditor;
 
 public class PointTest {
 
@@ -15,11 +14,17 @@ public class PointTest {
 
         GeometryFactory geometryFactory = new GeometryFactory(precisionModel, 4326);
 
-        Point point1 = geometryFactory.createPoint();
+        CoordinateArraySequenceFactory instance = CoordinateArraySequenceFactory.instance();
+
+
+        Point point1 = geometryFactory.createPoint(coordinate);
         System.out.println(point1);
         Point point = new Point(coordinateArraySequence, geometryFactory);
 
         System.out.println(point.toText());
+        System.out.println(point.equals(point1));
+
+        //postgresql - postgis와 연동하는 방법 처리 테스트해보기.
     }
 
 }
