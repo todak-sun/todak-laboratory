b7eb38047943269d0aec7d15a540618a07d53ed0
todak-sun <tjsdydwn@gmail.com>
2021-09-11 23:28:10 +0900

    mongo domain definition

---
+++ spring-boot-mongo/src/main/java/com/todak/laboratory/study/springbootmongo/Post.java
@@ -0,0 +1,20 @@
+package com.todak.laboratory.study.springbootmongo;
+
+import java.time.LocalDateTime;
+
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String author;
+
+    private LocalDateTime writtenAt;
+
+    private LocalDateTime updatedAt;
+
+
+}
