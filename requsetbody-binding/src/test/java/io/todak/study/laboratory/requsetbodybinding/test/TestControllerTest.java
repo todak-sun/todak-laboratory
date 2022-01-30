12b016e29915edda933c925d358e7ce86988124f
todak-sun <tjsdydwn@gmail.com>
2022-01-30 22:35:08 +0900

    만들면서 배우는 클린아키텍쳐 따라힉

--- requsetbody-binding/src/test/java/io/todak/study/laboratory/requsetbodybinding/test/TestControllerTest.java
+++ requsetbody-binding/src/test/java/io/todak/study/laboratory/requsetbodybinding/test/TestControllerTest.java
@@ -1,10 +1,19 @@
 package io.todak.study.laboratory.requsetbodybinding.test;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,8 +24,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TestControllerTest {
@@ -47,6 +55,7 @@ class TestControllerTest {
         );
     }
 
+
     @Test
     @DisplayName("기본 생성자가 없는 클래스 타입에 역직렬화가 성공한다.")
     void test2() throws Exception {
@@ -65,6 +74,49 @@ class TestControllerTest {
         TestController.WithCustomConstructor deserializeTarget = this.objectMapper.readValue(serializedValue, TestController.WithCustomConstructor.class);
 
         // then
+        assertNotNull(deserializeTarget.getId());
+        assertNotNull(deserializeTarget.getName());
+        assertNotNull(deserializeTarget.getAddress());
+
+        assertEquals(Long.valueOf(id), deserializeTarget.getId());
+        assertEquals(name, deserializeTarget.getName());
+        assertEquals(address, deserializeTarget.getAddress());
+    }
+
+    @Test
+    @DisplayName("스프링이 제공해주는 ObjectMapper의 인스턴스와, 기본 생성자를 통해 생성한 ObjectMapper 인스턴스 비교")
+    void test3() throws Exception {
+        // given
+        String id = "1";
+        String name = "todak";
+        String address = "seoul";
+
+        var data = new HashMap<String, String>();
+        data.put("id", id);
+        data.put("name", name);
+        data.put("address", address);
+
+        var objectMapper = new ObjectMapper();
+
+        // when
+        String serializedValue = objectMapper.writeValueAsString(data);
+        assertThrows(InvalidDefinitionException.class, () -> {
+            objectMapper.readValue(serializedValue, TestController.WithCustomConstructor.class);
+        });
+
+        objectMapper.registerModule(new SimpleModule());
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
+        objectMapper.registerModule(new JsonComponentModule());
+        objectMapper.setVisibility(new VisibilityChecker.Std(JsonAutoDetect.Visibility.PUBLIC_ONLY, JsonAutoDetect.Visibility.PUBLIC_ONLY, JsonAutoDetect.Visibility.ANY, JsonAutoDetect.Visibility.ANY, JsonAutoDetect.Visibility.PUBLIC_ONLY));
+        objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.PUBLIC_ONLY);
+        objectMapper.setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.PUBLIC_ONLY);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        ObjectMapper springBean =  this.objectMapper;
+
+        TestController.WithCustomConstructor deserializeTarget = objectMapper.readValue(serializedValue, TestController.WithCustomConstructor.class);
+
         assertNotNull(deserializeTarget.getId());
         assertNotNull(deserializeTarget.getName());
         assertNotNull(deserializeTarget.getAddress());
